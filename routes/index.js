bb8d0fbf60881d31fc737aca16d81cec4651ca79
LDic <vo2qjsld@gmail.com>
2019-04-03 20:57:49 +0900

    Implement basic functions
    homepage, auth

---
+++ routes/index.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+var path = require('path');
+router.use(bodyParser.urlencoded({extended: false}));
+
+/** Home Page */
+router.get(['/', '/start'], function(req, res, next) {
+  if(req.session.bIsLogined) // login already
+  {
+    res.redirect('/dashboard');
+  }
+  else
+  {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  }
+});
+
+router.get('/dashboard', function(req, res, next)
+{
+  if(!req.session.bIsLogined)
+  {
+    res.redirect('/');
+    return false;
+  }
+  console.log(req.session);
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+})
+
+/** Search & Result Page */
+router.get('/summary', function(req, res, next)  // GET summary page
+{
+  // Access control
+  if(!req.session.bIsLogined)
+  {
+    res.redirect('/');
+    return false;
+  }
+  console.log('summary page');
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+router.post('/summary', function(req, res, next) // POST summary page
+{
+  // Access control
+  if(!req.session.bIsLogined)
+  {
+    res.redirect('/');
+    return false;
+  }
+  console.log('post summary page');
+  // Show Summary
+});
+
+router.get('/search', function(req, res, next)  // GET search page
+{
+  // Access control
+  if(!req.session.bIsLogined)
+  {
+    res.redirect('/');
+    return false;
+  }
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+// Send username data to frontend
+router.post('/search', function(req, res, next)
+{
+  // Access control
+  if(!req.session.bIsLogined)
+  {
+    res.redirect('/');
+    return false;
+  }
+});
+
+module.exports = router;
